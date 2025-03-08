71a0ef9ea974eaa457ac7fa290a0b2906618f918
Erick Medeiros <erickb_m@hotmail.com>
2025-03-08 11:15:07 -0300

    02 - updates de informações de arquivos tf + adição do arquivo output.tf

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "resource_group_name_eastus" {
+  value = azurerm_resource_group.eastus_rg.name
+}
+
+output "resource_group_name_brazilsouth" {
+  value = azurerm_resource_group.brazilsouth_rg.name
+}
