a5a7b4e88a2477e1bbe5fbef9627aa4d65c1561c
Erick Medeiros <erickb_m@hotmail.com>
2025-03-08 11:39:54 -0300

    04 - updates de informações de arquivos tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 
 ########### BLOCO DE VARIAVEIS ######
 variable "azure_region_eastus" {
- type = string
- default = "eastus" 
- description = "description"
+  type        = string
+  default     = "eastus"
+  description = "description"
 }
 
 variable "resource_group_name_eastus" {
-  type = string
-  default = "eastus_rg"
+  type        = string
+  default     = "eastus_rg"
   description = "description"
 }
